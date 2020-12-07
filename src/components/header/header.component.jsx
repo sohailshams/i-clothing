cab55317930a3b2400bbe61aa4a6f62ef633f2ad
sohailshams <sohailshamas@yahoo.com>
2020-12-07 15:30:35 +0100

    Added cart toggle functionality on click

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -14,7 +14,7 @@ import CartDropdown from '../cart-dropdown/card-dropdown-component';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -34,11 +34,14 @@ const Header = ({ currentUser }) => (
             )}
             <CartIcon />
         </div>
-            <CartDropdown />
+        {
+            hidden ? null : <CartDropdown />
+        }
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 })
 export default connect(mapStateToProps)(Header);
