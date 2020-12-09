ba37ddb6dfb1973e9257767156115e99b6112466
sohailshams <sohailshamas@yahoo.com>
2020-12-09 23:31:27 +0100

    Added checkout-item component to display items on checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 
 import { createStructuredSelector } from 'reselect';
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
@@ -28,9 +30,9 @@ const CheckoutPage = ({cartItems, total}) => (
             </div>
         </div>   
         {
-            cartItems.map(cartItem =>
-                cartItem.name
-                )
+            cartItems.map(cartItem =>(
+                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+            ))
         }
         <div className='total'>
     <span>Total: ${total}</span>
