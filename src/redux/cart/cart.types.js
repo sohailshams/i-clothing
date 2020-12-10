3248a0e62df0bc386543fb32c8220ef025b75acb
sohailshams <sohailshamas@yahoo.com>
2020-12-10 09:27:13 +0100

    Added remove item functionality

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,7 @@
 const CartActionTypes = {
   TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
   ADD_ITEM: 'ADD_ITEM',
+  CLEAT_ITEM_FROM_CART: 'CLEAT_ITEM_FROM_CART',
 };
 
 export default CartActionTypes;
