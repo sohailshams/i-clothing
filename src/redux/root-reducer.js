2fac6f5dc79df019ad7fd9970b67af9f4f1c3a46
sohailshams <sohailshamas@yahoo.com>
2020-12-11 11:41:54 +0100

    Moved dictory into redux-created directory reducer and selectors

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -8,6 +8,8 @@ import userReducer from './user/user.reducer';
 
 import cartReducer from './cart/cart.reducer';
 
+import directoryReducer from './directory/directory.reducer';
+
 const persistConfig = {
   key: 'root',
   storage,
@@ -17,6 +19,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
