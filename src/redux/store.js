c929baf0abf2aef10924cb2ff94ccf11bb713eeb
sohailshams <sohailshamas@yahoo.com>
2020-12-17 19:43:00 +0100

    Added async event handling with redux functionality and created collection container and collection-overview container components

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,9 @@ import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [];
+import thunk from 'redux-thunk';
+
+const middlewares = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
