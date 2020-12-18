fcb61d363e756c49bd09783ac51a3a3e9757bd2a
sohailshams <sohailshamas@yahoo.com>
2020-12-18 11:43:46 +0100

    Installed saga library and moved code from async action using thunk into saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,15 +6,20 @@ import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
-import thunk from 'redux-thunk';
+import { fetchCollectionsStart } from './shop/shop.sagas';
 
-const middlewares = [thunk];
+import createSagaMiddleware from 'redux-saga';
+
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionsStart);
 
 export const persistor = persistStore(store);
 
